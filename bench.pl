72f16ccdf23e1b17385b55975ba72df977dbb1a7
Ricardo Signes <rjbs@cpan.org>
2009-11-16 11:54:32 -0500

    quick benchmark

---
+++ bench.pl
@@ -0,0 +1,27 @@
+use strict;
+use 5.010;
+use lib 'lib';
+
+use Benchmark;
+use String::Format;
+use String::Formatter;
+
+my $fmt = String::Formatter->new({
+  codes => {
+    a => "apples",
+    b => "bannanas",
+  },
+});
+
+my $index_format = String::Format->stringfactory({
+  a => "apples",
+  b => "bannanas",
+});
+
+say $index_format->("I like to eat %a and %b.");
+say $fmt->format("I like to eat %a and %b.");
+
+timethese(100_000, {
+  dlc  => sub { $index_format->("I like to eat %a and %b.") },
+  rjbs => sub { $fmt->format("I like to eat %a and %b.") },
+});
